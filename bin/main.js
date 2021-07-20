a795b205c5ad78143d47e7d0d8f6c926e8223d87
Oops <857901398@qq.com>
2021-07-21 00:10:12 +0800

    new

---
+++ bin/main.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+console.log("bin...", process.cwd());
+
+const path = require("path");
+
+// config 配置文件
+const config = require(path.resolve('webpack.config.js'));
+
+
+const Compiler = require("../lib/Compiler");
+
+const compiler = new Compiler(config);
+
+compiler.run();
