711e20e97da38909083830e30d3af3c613115c6b
Oops <857901398@qq.com>
2021-07-21 18:10:24 +0800

    loader plugins

--- bin/main.js
+++ bin/main.js
@@ -1,15 +1,17 @@
 #! /usr/bin/env node
 
-console.log("bin...", process.cwd());
+console.log('bin...', process.cwd());
 
-const path = require("path");
+const path = require('path');
 
 // config 配置文件
 const config = require(path.resolve('webpack.config.js'));
 
-
-const Compiler = require("../lib/Compiler");
+const Compiler = require('../lib/Compiler');
 
 const compiler = new Compiler(config);
+
+// 调用 hook
+compiler.hooks.entryOption.call();
 
 compiler.run();
